5a76b87770bf06429872465db1a41e7ce51ed7c2
Pskdr <juanca.naranjo03@gmail.com>
2023-08-08 16:14:37 -0500

    feat: adding new articles and new tab

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -21,7 +21,7 @@ const Navbar = ({ setMostrar, mostrar, steps }) => {
   };
   const containerStyle = {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))',
     gap: '20px',
     padding: '20px',
     marginBottom: '20px',
